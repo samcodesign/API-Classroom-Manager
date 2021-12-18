ebb79c5cef8dc5a5ba19f7eaa0ca7bef78c19586
Samy <46052152+samcodesign@users.noreply.github.com>
2021-12-18 12:09:40 +0000

    Co-authored-by: Moncef037 <Moncef037@users.noreply.github.com>

--- data.py
+++ data.py
@@ -6,3 +6,7 @@ with open("data/teachers_test.json", 'r') as read_file:
 
 with open("json/salles.json", 'r') as read_file:
     salles_TD = json.load(read_file)
+
+
+
+description = 'Ceci est prototype D\'API qui concerne la gestion ainsi que la réservation d\'une ou plusieurs salles td de l\'Université des sciences et technologies Houari Boumedienne'
--- main.py
+++ main.py
@@ -2,21 +2,16 @@ from fastapi import FastAPI
 from typing import Optional
 from data import teachers_data as Td
 from data import salles_TD as Sd
-
+from data import description
 
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def index():
+    return description
 
-@app.get("/sallesTD_Test")
-def sallesTest():
-    return Sd
-
-
-@app.get("/Teachers_data")
-def TeachersDataTest():
-    return Td
+@app.get("/testing_data")
+def TestingData():
+    return 
