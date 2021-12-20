8e88ea40ba5da5ab42c299603c29b3d05e0aabf8
Samy <46052152+samcodesign@users.noreply.github.com>
2021-12-20 22:31:49 +0000

    updating salles.py

--- salles.py
+++ salles.py
@@ -1,10 +1,114 @@
 sallesTD = {
     1: {
         "number": 100,
-        "reservedBy": ""
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
+
+
     },
     2: {
-        "number": 200,
-        "reservedBy": ""
+        "number": 101,
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
     },
+    3: {
+        "number": 102,
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
+
+    },
+    4: {
+        "number": 103,
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
+
+    },
+    5: {
+        "number": 200,
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
+
+    },
+    6: {
+        "number": 202,
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
+
+    },
+    7: {
+        "number": 205,
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
+
+    },
+    8: {
+        "number": 300,
+        "reservedBy": "",
+        "créneaux": {
+            "crénau1": "8:00_9:30",
+            "crénau2": "9:30_11:00",
+            "crénau3": "11:00_12:30",
+            "crénau4": "13:00_14:30",
+            "crénau5": "14:30_16:00",
+
+        },
+        "module": ["Math", "ELN", "ELT", "VOM"]
+
+    }
 }
