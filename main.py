d3c04c8497b61e06927e69b2f37ace27e7143249
Samy <46052152+samcodesign@users.noreply.github.com>
2021-12-21 13:46:26 +0000

    updating vars & adding put

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ from fastapi import FastAPI, Path
 from typing import Optional
 import json
 from salles import sallesTD
+from teachers_data import teachers_data
+from pydantic import BaseModel
 
 
-with open("json/teachers_test.json", 'r') as read_file:
-    teachers_data = json.load(read_file)
-#with open("json/salles.json", 'r') as read_file:
+# with open("json/teachers_test.json", 'r') as read_file:
+#    teachers_data = json.load(read_file)
+# with open("json/salles.json", 'r') as read_file:
 #   sallesTD = json.load(read_file)
-
 description = 'Ceci est prototype D\'API qui concerne la gestion ainsi que la réservation d\'une ou plusieurs salles td de l\'Université des sciences et technologies Houari Boumedienne'
 
+class Teacher(BaseModel):
+    First_name : str
+    Last_name: str
+    Password: str
+    email: str
 
-
+class UpdateTeacher(BaseModel):
+    First_name: Optional[str] = None
+    Last_name: Optional[str] = None
+    Password: Optional[str] = None
+    email: Optional[str] =  None
 
 app = FastAPI()
 
@@ -43,3 +53,28 @@ def get_classNumber(num: int):
         if sallesTD[sallesTD_id]["number"] == num:
             return sallesTD[sallesTD_id]
     return {"Data": "Not found"}
+
+
+@app.get("/get-by-name")
+def getName(*, name: Optional[str] = None, test: int):
+    for teacher_id in teachers_data:
+        if teachers_data[teacher_id]["Last_name"] == name:
+            return teachers_data[teacher_id]
+    return {"Data": "Not Found"}
+
+@app.post("/create-teacher/{teacher_id}")
+def CreateTeacher(teacher_id:int, teacher: Teacher):
+    if teacher_id in teachers_data:
+        return {"Error": "Teacher already exist in database"}
+    
+    teachers_data[teacher_id] = teacher
+    return teachers_data[teacher_id]
+
+
+@app.put("/update-teacher/{teacher_id}")
+def updateTeacher(teacher_id : int, teacher: UpdateTeacher):
+    if teacher_id in teachers_data:
+        return {"Error": "Teacher does not exist in database"}
+    
+    teachers_data[teacher_id] = teacher
+    return teachers_data[teacher_id]
---
+++ teachers_data.py
@@ -0,0 +1,29 @@
+teachers_data = {
+    1: {
+        "First_name": "Ahmed",
+        "Last_name": "Mougari",
+        "Password": "qwerty",
+        "email": "mail@yourmail.com"
+    },
+    2: {
+        "First_name": "Omar",
+        "Last_name": "Chaterbache",
+        "Password": "q012345",
+        "email": "mail@yourmail.com"
+
+    },
+    3: {
+        "First_name": "Moufek",
+        "Last_name": "Hamza",
+        "Password": "2+2is4",
+        "email": "mail@yourmail.com"
+
+    },
+    4: {
+        "First_name": "Ghoumari",
+        "Last_name": "Leila",
+        "Password": "samy",
+        "email": "mail@yourmail.com"
+
+    }
+}
